4dc0af3ac323578013967850e50e0f79d763e637
Philipp Michelfeit <philippm1988@users.noreply.github.com>
2023-03-09 16:36:24 +0100

    corrected method isPalindrome2

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println( Palindrome.isPalindrome1("Moon"));
+        System.out.println( Palindrome.isPalindrome1("Noon"));
+        System.out.println( Palindrome.isPalindrome2("Moon"));
+        System.out.println( Palindrome.isPalindrome2("Noon"));
+    }
+}
--- palindrome.java
+++ palindrome.java
@@ -1,22 +1,26 @@
-// Möglichkeit 1
-public static boolean isPalindrome(final String text) {
-   final String str = text.toLowerCase();
-   int forward = 0;
-   int backward = str.length() - 1;
-   while (backward > forward) {
-       char forwardChar = str.charAt(forward++);
-       char backwardChar = str.charAt(backward--);
-             
-       if (forwardChar != backwardChar)
-           return false;
-   }
-   return true;
+class Palindrome {
+    // Solution 1
+    public static boolean isPalindrome1(final String text) {
+        final String str = text.toLowerCase();
+        int forward = 0;
+        int backward = str.length() - 1;
+        while (backward > forward) {
+            char forwardChar = str.charAt(forward++);
+            char backwardChar = str.charAt(backward--);
+
+            if (forwardChar != backwardChar)
+                return false;
+        }
+        return true;
+    }
+
+    // Solution 2
+    public static boolean isPalindrome2(final String text) {
+        final String str = text.toLowerCase();
+        final StringBuilder original = new StringBuilder(str);
+        final StringBuilder reverse = original.reverse();
+        return reverse.toString().equals(str);
+    }
 }
 
-// Möglichkeit 2
-public static boolean isPalindrome(final String text) {
-    final StringBuilder original = new StringBuilder(text.toLowerCase());
-    final StringBuilder reverted = original.reverse();
 
-    return original.equals(reverted);
-}
